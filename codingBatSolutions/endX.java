f7b3267cde0d6ad8bfd6f1c6aa99489b63b2a845
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2021-11-17 22:49:44 -0500

    Create endX.java

---
+++ codingBatSolutions/endX.java
@@ -0,0 +1,6 @@
+// Recursion-1
+public String endX(String str) {
+  if(str.length()==0) return str;
+  if(str.charAt(0)=='x') return endX(str.substring(1)) + "x";
+  return str.charAt(0)+endX(str.substring(1));
+}
