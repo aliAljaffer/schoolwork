2d53dcdcc015806888ad4bf8b7d2b06c953b4fe8
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2021-12-02 15:22:43 -0500

    Create strDist.java

---
+++ codingBatSolutions/strDist.java
@@ -0,0 +1,7 @@
+// Recursion-1
+public int strDist(String str, String sub) {
+  if(str.length()==0) return 0;
+  if(!str.startsWith(sub)) return strDist(str.substring(1),sub);
+  if(!str.endsWith(sub)) return strDist(str.substring(0,str.length()-1),sub);
+  return str.length();
+}
