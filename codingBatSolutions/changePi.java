094123cff83f480a203dac86e685a1f551ebfe21
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2022-01-27 13:04:50 -0500

    Create changePi.java

---
+++ codingBatSolutions/changePi.java
@@ -0,0 +1,5 @@
+public String changePi(String str) {
+  if(str.length()<2) return str;
+  if(str.startsWith("pi")) return "3.14" + changePi(str.substring(2));
+  return str.charAt(0) + changePi(str.substring(1));
+}
