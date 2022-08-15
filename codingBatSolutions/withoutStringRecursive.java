a03e71777fdf52a570b3f9df63c31bee099f227c
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2022-08-15 13:24:54 -0400

    Update withoutStringRecursive.java

--- codingBatSolutions/withoutStringRecursive.java
+++ codingBatSolutions/withoutStringRecursive.java
@@ -6,5 +6,5 @@ public String withoutString(String base, String remove) {
     if (removeCandidate.equalsIgnoreCase(remove)) {
       return withoutString(base.substring(remove.length()), remove);
     }
-    return hey.charAt(0) + withoutString(base.substring(1), remove);
+    return removeCandidate.charAt(0) + withoutString(base.substring(1), remove);
 }
